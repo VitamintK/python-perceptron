1583ab620822d91c9542f23237a9e1b96e48a277
KEvin Wang <kevinwang@kevinwang.us>
2015-08-26 03:18:15 -0700

    Created a working perceptron, and plotting
    perceptron works for d = 2 (two dimensions), and plotting and saving the
    perceptron model to mp4 from matplotlib works as well.

--- perceptron.py
+++ perceptron.py
@@ -1,2 +1,99 @@
 import random
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+from collections import namedtuple
+
+class Perceptron:
+	def __init__(self, sample = None, dimensions = 2):
+		self.d = dimensions
+		self.w = np.zeros(dimensions)
+		self.sample = sample
+		self.b = 0
+		self.done = False
+	def iterate(self, plot = False):
+		random.shuffle(self.sample)
+		for x in self.sample:
+			if (np.dot(x.x, self.w) > -self.b) != x.y:
+				self.w = self.w + ((x.y)*2-1) * x.x
+				self.b += (x.y)*2 - 1
+				print(self.w, self.b)
+				break
+		else:
+			print('we done here fAM')
+			self.done = True
+			return False
+		if plot:
+			self.plot()
+			return True
+	def learn(self, plot=False):
+		while self.iterate(plot):
+			pass
+	def plot(self):
+		plt.clf()
+		plt.axis([-1,1,-1,1])
+		for x in self.sample:
+			if (np.dot(x.x, self.w) >= -self.b) != x.y:
+			#if x.y != 1:
+				plt.plot(x.x[0], x.x[1], 'or')
+			else:
+				plt.plot(x.x[0], x.x[1], 'ob')
+		n = np.linalg.norm(self.w)
+		if self.w[1] != 0:
+			ww = 1.25 * self.w/n
+			m = -self.w[0]/self.w[1]
+			b = -self.b/self.w[1]
+			l = np.linspace(-1,1)
+			plt.plot(l, m*l + b, '-k')
+		#plt.ion()
+		#plt.show()
+Point = namedtuple('Point', ['y', 'x'])
+
+class Separable_Data:
+	def __init__(self, n = 1000, f = None, dimensions = 2, bounds = None):
+		if bounds == None:
+			bounds = [(-1,1) for _ in range(dimensions)]
+		if f == None:
+			f = self.make_f()
+		self.d = dimensions
+		self.bounds = bounds
+		self.f = f
+		self.data = []
+		self.generate_points(n)
+	def make_f(self):
+		up = random.randint(0,1)
+		x = random.random()*2 - 1
+		y = random.random()*2 - 1
+		m = math.tan(random.random() * 2 * 3.14)
+		print("m = {}, b = {}".format(m, -m*x + y))
+		if up:
+			return lambda x0, x1:  x1 - m*x0 < -m*x + y
+		else:
+			return lambda x0, x1:  x1 - m*x0 > -m*x + y
+	def generate_points(self, n):
+		for i in range(n):
+			point = np.array([random.uniform(self.bounds[j][0], self.bounds[j][1]) for j in range(self.d)])
+			self.data.append(Point(self.f(*point), point))
+	def produce_sample(self, n):
+		return random.sample(self.data, n)
+
+
+plt.axis([-1,1,-1,1])
+data = Separable_Data().produce_sample(40)
+p = Perceptron(sample = data)
+
+fig = plt.figure()
+def animate(i):
+	p.iterate(plot=True)
+def g():
+	while not p.done:
+		print('yay')
+		yield True
+	for _ in range(0,3):
+		yield True
+ani = animation.FuncAnimation(fig, animate, frames = g(), interval = 1000)
+ani.save('PERCEPTRON.mp4', fps=2)#, extra_args=['-vcodec', 'libx264'])
+print("we on this line now")
+plt.show()
+print("we finish the whole thing")
